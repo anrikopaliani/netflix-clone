0f3a8e27dc5e088d6783bc55a098f8f495f0c759
kopala123 <anri.kopaliani.1@btu.edu.ge>
2024-02-20 00:48:44 +0400

    feat:create pages based on genre

--- app/components/MovieVideo.tsx
+++ app/components/MovieVideo.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import prisma from "../utils/db";
+import MovieButtons from "./MovieButtons";
 const getData = async () => {
   const data = await prisma.movie.findFirst({
     select: {
@@ -11,6 +12,7 @@ const getData = async () => {
       duration: true,
       id: true,
       age: true,
+      youtubeString: true,
     },
   });
 
@@ -36,8 +38,16 @@ const MovieVideo = async () => {
         </h1>
         <p className="text-white text-lg mt-5 line-clamp-3">{data?.overview}</p>
         <div className="flex gap-x-3 mt-4">
-          <Button>See more</Button>
-          <Button>Learn more</Button>
+          <MovieButtons
+            age={data?.age as number}
+            duration={data?.duration as number}
+            id={data?.id as number}
+            overview={data?.overview as string}
+            releaseDate={data?.release as number}
+            title={data?.title as string}
+            youtubeUrl={data?.youtubeString as string}
+            key={data?.id}
+          />
         </div>
       </div>
     </div>
---
+++ app/home/[genre]/page.tsx
@@ -0,0 +1,130 @@
+import MovieCard from "@/app/components/MovieCard";
+import { authOptions } from "@/app/utils/auth";
+import prisma from "@/app/utils/db";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+const getData = async (category: string, userId: string) => {
+  switch (category) {
+    case "shows": {
+      const data = await prisma.movie.findMany({
+        where: {
+          category: "show",
+        },
+        select: {
+          age: true,
+          duration: true,
+          id: true,
+          release: true,
+          imageString: true,
+          overview: true,
+          youtubeString: true,
+          title: true,
+          WatchLists: {
+            where: {
+              userId: userId,
+            },
+          },
+        },
+      });
+      return data;
+    }
+    case "movies": {
+      const data = await prisma.movie.findMany({
+        where: {
+          category: "movie",
+        },
+        select: {
+          age: true,
+          duration: true,
+          release: true,
+          id: true,
+          imageString: true,
+          overview: true,
+          youtubeString: true,
+          title: true,
+          WatchLists: {
+            where: {
+              userId: userId,
+            },
+          },
+        },
+      });
+
+      return data;
+    }
+    case "recently": {
+      const data = await prisma.movie.findMany({
+        where: {
+          category: "recent",
+        },
+        select: {
+          age: true,
+          duration: true,
+          release: true,
+          id: true,
+          imageString: true,
+          overview: true,
+          youtubeString: true,
+          title: true,
+          WatchLists: {
+            where: {
+              userId: userId,
+            },
+          },
+        },
+      });
+      return data;
+    }
+
+    default: {
+      return;
+    }
+  }
+};
+
+const CategoryPage = async ({ params }: { params: { genre: string } }) => {
+  const session = await getServerSession(authOptions);
+  const data = await getData(params.genre, "abc");
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-5 sm:px-0 mt-0 gap-6">
+      {data?.map((movie) => (
+        <div key={movie.id} className="relative h-60">
+          <Image
+            src={movie.imageString}
+            width={500}
+            height={400}
+            className="rounded absolute w-full h-full object-cover"
+            alt="Movie"
+          />
+          <div className="h-60 relative z-10 w-full transform transition duration-500 hover:scale-125 opacity-0 hover:opacity-100">
+            <div className="bg-grandient-to-b from-transparent via-black/50 to-black z-10 w-full h-full  rounded-lg flex items-center justify-center">
+              <Image
+                src={movie.imageString}
+                width={800}
+                height={800}
+                className=" absolute w-full h-full -z-10 rounded-lg object-cover brightness-50 "
+                alt="Movie"
+              />
+
+              <MovieCard
+                key={movie.id}
+                age={movie.age}
+                movieId={movie.id}
+                overview={movie.overview}
+                time={movie.duration}
+                title={movie.title}
+                year={movie.release}
+                youtubeUrl={movie.youtubeString}
+                watchListId={movie.WatchLists[0]?.id}
+                watchList={movie.WatchLists.length > 0 ? true : false}
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CategoryPage;
